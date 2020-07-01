aeaf2608b2000d5c88a1112f5561962cabdddcd4
Joe Webb <webbdeveloping@gmail.com>
2020-07-01 15:34:37 -0600

    fixed the about hero

--- src/components/sections/HeroNoImg.js
+++ src/components/sections/HeroNoImg.js
@@ -30,27 +30,27 @@ const HeroNoImg = ({
   );
 
   const innerClasses = classNames(
-    'hero-inner section-inner flex-row-start ml-32',
+    'hero-inner section-inner flex-row-start ml-32 mb-0 pb-0',
     topDivider && 'has-top-divider',
     bottomDivider && 'has-bottom-divider'
   );
 
   return (
     <section {...props} className={outerClasses}>
-
-        <div className={innerClasses}>
-          <div className='ml-32 pl-32 fs-80'>
-            <h1
-              className='mt-0 mb-16 reveal-from-left fs-80 cta-col p-32'
-              data-reveal-delay='200'
-            >
-              ABOUT
-               {/* <span className='text-color-primary fs-80'>XBanc</span> */}
-            </h1>
-          </div>
+      <div className={innerClasses}>
+        <div className='ml-32 pl-32'>
+          <h1
+            className='mt-0 reveal-from-left fs-80 cta-col p-32 mb-0'
+            data-reveal-delay='200'
+          >
+            ABOUT
+            {/* <span className='text-color-primary fs-80'>XBanc</span> */}
+          </h1>
         </div>
-        <hr />
-        <h4>ALL ABOUT XBANC</h4>
+      </div>
+      <div className='w-100 border-t-b ml-32'>
+        <h4 className='fs-14'>OUR STORY IS EPIC</h4>
+      </div>
     </section>
   );
 };
